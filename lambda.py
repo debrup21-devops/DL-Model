907a78c1e7f83bc1f99eca23f7db517a1f0a51dd
debrup21-devops <110044147+debrup21-devops@users.noreply.github.com>
2022-12-06 18:05:48 +0530

    Update lambda.py
    Signed-off-by: debrup21-devops <110044147+debrup21-devops@users.noreply.github.com>

--- lambda.py
+++ lambda.py
@@ -12,4 +12,6 @@ def lambda_handler(event, context):
 #     image = open(s3_files[0], "rb")
     file_path = "https://dl-model-aws-connection-image-bucket.s3.eu-west-2.amazonaws.com/track.jpeg"
     data  = subprocess.run(["python3", "detect.py", "--weights", "yolov5x.pt", "--source", file_path], capture_output=True)
-    return json.dumps(data)
+    print(data)
+    
+    return json.dumps({"result": data})
